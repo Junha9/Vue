ce85e0ee5564bdec5137c8571a77ab6dc6ce28a7
Junha9 <ghno20@gmail.com>
2022-11-14 17:46:36 +0900

    1114

---
+++ todo-vuex-app/src/components/TodoListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span @click="updateTodoStatus"
+    :class="{ 'is-completed': todo.isCompleted }"
+    > {{ todo.title }} </span>
+    <button @click="deleteTodo">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem',
+  props: {
+    todo: Object,
+  },
+  methods :{
+    deleteTodo() {
+      console.log('Delete Todo')
+      this.$store.dispatch('deleteTodo', this.todo)
+    },
+    updateTodoStatus() {
+      this.$store.dispatch('updateTodoStatus', this.todo)
+    },
+  },
+}
+</script>
---
+++ vue-router-app/src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input type="text"
+    @keyup.enter="goToHell"
+    v-model="inputData"
+    >
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      inputData : null,
+    }
+  },
+  methods : {
+    goToHell() {
+      this.$router.push({ name:'hello', params: { userName: this.inputData } })
+    },
+  },
+}
+
+</script>
